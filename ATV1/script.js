27dfe9028c1d7d9cb3ce4064d446a783ea52a6c9
Guilherme Melnik Trombetta <126004347+Flerno14@users.noreply.github.com>
2023-08-10 08:59:06 -0300

    Delete ATV1 directory

--- ATV1/script.js
+++
@@ -1,9 +0,0 @@
-function Parilidade(){
-        let Número = Number(document.getElementById("entrada").value)   //Esta parte é a responsável por receber o valor do input no HTML
-        
-        if (Número % 2 === 0){    //se o resto da divisão do npumero obtido for = 0 ele envia um alerta de PAR
-          alert("PAR");
-        } else {                   //se não ele envia um alerta de impar
-          alert("IMPAR");
-        }
-    }
